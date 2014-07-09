c6d4c5a27bf1efa317e53160462b9818833c258b
zaemin2 <zaemin2@gmail.com>
2014-07-09 09:32:39 +0900

    base with action bar

--- src/jp/co/supersoftware/gourmet/MainActivity.java
+++ src/jp/co/supersoftware/gourmet/MainActivity.java
@@ -2,8 +2,6 @@ package jp.co.supersoftware.gourmet;
 
 import java.util.Locale;
 
-import android.support.v7.app.ActionBar.Tab;
-import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -12,13 +10,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
@@ -76,9 +69,9 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
             // this tab is selected.
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setIcon(mSectionsPagerAdapter.getPageIcon(i))
-                            .setTabListener(this));
+//                   .setText(mSectionsPagerAdapter.getPageTitle(i))
+                    .setIcon(mSectionsPagerAdapter.getPageIcon(i))
+                    .setTabListener(this));
         }
     }
     
@@ -124,8 +117,8 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-    	static final int FRAGMENT_CNT = 3;	// Show 3 total pages.
-    	final int[] ICON = new int[] {
+    	final int FRAGMENT_CNT = 3;	// Show 3 total pages.
+    	final int[] ICON = new int[] {		// ? is not available inner class ?
             R.drawable.ic_search,
             R.drawable.ic_coopon,
             R.drawable.ic_social,
@@ -138,8 +131,12 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // Return a PlaceholderFragment (defined as a static inner class below).            
+            switch(position) {
+            	case 0:	return FirstFragment.newInstance(position + 1);
+            	case 1: return SecondFragment.newInstance(position + 1);
+            	default:return ThirdFragment.newInstance(position + 1);
+            }
         }
 
         @Override
@@ -166,40 +163,4 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         	return ICON[position];
         }
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
 }
